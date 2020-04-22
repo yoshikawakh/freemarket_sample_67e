161ac8a6a6c85a91428b36b2a109ef9123bc65dd
kazuya0507 <fas0507@outlook.jp>
2020-04-22 20:13:18 +0900

    product_listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/new'
   root to: 'toppages#index'
   resources :users, only: [:index]
   resources :creditcards, only: [:new]
+  resources :products, only: [:new]
 end
