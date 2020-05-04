668fcee2abffdaa88b2d1e5e8fd0bb55a0b4b3fb
misatone1989 <misa1201dasu@yahoo.co.jp>
2020-05-04 19:57:29 +0900

    first_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-  }
-  devise_scope :user do
-    get 'addresses', to: 'users/registrations#new_address'
-    post 'addresses', to: 'users/registrations#create_address'
-  end
-  root to: 'toppages#index'
-  resources :users, only: [:index] do
-    collection do
-      get 'logout'
-    end
-  end
-  resources :creditcards, only: [:new]
-  resources :products, only: [:new]
+  # devise_for :users, controllers: {
+  #   registrations: 'users/registrations',
+  # }
+  # devise_scope :user do
+  #   get 'addresses', to: 'users/registrations#new_address'
+  #   post 'addresses', to: 'users/registrations#create_address'
+  # end
+  # root to: 'toppages#index'
+  # resources :users, only: [:index] do
+  #   collection do
+  #     get 'logout'
+  #   end
+  # end
+  # resources :creditcards, only: [:new]
+  # resources :products, only: [:new] do
+  #   member do
+  #     get 'check'
+  #   end
+  # end
+  root to: 'products#check'
 end
