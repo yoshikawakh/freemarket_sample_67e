bc1e481e12a9e4450dc99f7f5f57d349f8fef1b3
programing-biginner <60635421+programing-biginner@users.noreply.github.com>
2020-05-26 22:25:25 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -47,15 +47,15 @@ ActiveRecord::Schema.define(version: 2020_05_16_075134) do
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "product_name"
-    t.integer "price"
+    t.string "product_name", null: false
+    t.integer "price", null: false
     t.string "size"
     t.string "status"
-    t.string "postage"
+    t.string "postage", null: false
     t.text "explanation"
     t.text "delivery_method"
-    t.string "delivery_origin"
-    t.string "arrival_date"
+    t.string "delivery_origin", null: false
+    t.string "arrival_date", null: false
     t.string "brand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
