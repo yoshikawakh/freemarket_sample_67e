f5d213a8058bb7967d0ec0f9bff1b9ba70d04618
kazuya0507 <fas0507@outlook.jp>
2020-05-31 11:23:26 +0900

    model_association

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   validates :images, length: { minimum: 1, maximum: 5}
-
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorites_products, through: :likes, source: :product
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,7 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true, format: { with: /\A[一-龥ぁ-ん]/}
   validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   has_one :address
+  has_many :favorites, dependent: :destroy
+  has_many :favorites_users, through: :favorites, source: :user
 
 end
