4366d8d8e4bb77be9a15c9c551e6178e96b1aea4
yoshikawakh <yoshikawakh@gmai.com>
2020-06-04 22:28:45 +0900

    エラーハンドリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,14 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @product = Product.find(params[:product_id])
-    @comment.save
-    redirect_to product_path(@product)
+    if @comment.save
+      redirect_to product_path(@product)
+    else
+      flash[:notice] = "コメント欄が空白です。"
+      redirect_to product_path(@product)
+    end
   end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, product_id: params[:product_id])
